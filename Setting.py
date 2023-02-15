e85f649b83167009d78ed8830adc1a52c53065b0
Bociojr <107802947+Bociojr@users.noreply.github.com>
2023-02-15 18:46:52 +0100

    Create Setting.py

---
+++ Setting.py
@@ -0,0 +1,11 @@
+nome_cane = fuffy
+anni_del_cane = 2
+favorite_food = snack
+
+
+print("hello world, i'm your bot dog")
+print("I'm finally alive thanks to you, what were you waiting for to install me?")
+print("my favorite food is"+favorite_food)
+print(\n "What is your name")
+nome_utente = imput()
+print("Hello" + nome_utente + "Pleased to meet you")
